bface65ffbb1db628e020e33cb927cc6e3ec9e06
unknown <berred.yassine@gmail.com>
2026-03-19 00:51:55 +0000

    fix styles vite

--- src/main.js
+++ src/main.js
@@ -9,15 +9,12 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
 const vuetify = createVuetify({
-  ssr: false,
   theme: {
     defaultTheme: 'light',
   },
   icons: {
     defaultSet: 'mdi', // This is already the default value - only for display purposes
   },
-  components,
-  directives
 })
 
 createApp(App)
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: true, // auto-imports Vuetify components
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  optimizeDeps: {
+    include: ['vuetify'],
+  },
+  ssr: {
+    noExternal: ['vuetify'], // critical for Vercel's build
+  },
+})
