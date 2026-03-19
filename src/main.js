8fb7cce597a1ec3616bbcf39d110632bd23a8394
unknown <berred.yassine@gmail.com>
2026-03-19 00:58:23 +0000

    fix styles 2

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ const vuetify = createVuetify({
   icons: {
     defaultSet: 'mdi', // This is already the default value - only for display purposes
   },
+  components,
+  directives
 })
 
 createApp(App)
